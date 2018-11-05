50fd17955116443c9ee7721a2f3d460dcd8082b0
Johannes Siipola <johannes@siipo.la>
2018-11-05 19:46:50 +0000

    Release v1.0.6

--- dominant-color-async.php
+++ dominant-color-async.php
@@ -5,11 +5,14 @@ Plugin Name: Dominant color async
 Plugin URI: https://github.com/CreunaFI/dominant-color-async
 Description: Calculate the dominant color for every image in WordPress, asynchronously
 Author: Johannes Siipola
-Version: 1.0.5
+Version: 1.0.6
 Author URI: https://siipo.la
 Text Domain: dominant-color-async
 */
 
-require 'vendor/autoload.php';
+// Check if we are using local Composer
+if (file_exists(__DIR__ . '/vendor')) {
+    require 'vendor/autoload.php';
+}
 
 new \DominantColorAsync\DominantColorAsync(plugin_basename(__FILE__), plugin_dir_path(__FILE__));
